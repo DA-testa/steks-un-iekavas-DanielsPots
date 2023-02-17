c61af345be10082aae4ac727b7eb8efa5f0dbd1b
DanielsPots <124889596+DanielsPots@users.noreply.github.com>
2023-02-17 13:19:39 +0000

    commit

--- main.py
+++ main.py
@@ -13,18 +13,36 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i + 1))
 
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
+        elif next in ")]}":
+            if not opening_brackets_stack:
+                return i + 1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i + 1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    else:
+        return "Success"
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    source = input("Choose 'F' to select a file or 'I' to input brackets: ")
+    if source.lower() == 'f':
+        file_path = input("Enter file path: ")
+        try:
+            with open(file_path, 'r') as f:
+                text = f.read()
+                mismatch = find_mismatch(text)
+                print(mismatch)
+        except FileNotFoundError:
+            print("File not found")
+    elif source.lower() == 'i':
+        text = input("Enter brackets: ")
+        mismatch = find_mismatch(text)
+        print(mismatch)
+    else:
+        print("Invalid input. Choose 'F' or 'I'.")
 
 
 if __name__ == "__main__":
